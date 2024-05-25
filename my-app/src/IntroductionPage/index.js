dcc8a6e5ccd9d9cd2744ef426a55a4b451471cf5
SuChenYu0623 <F110154121@nkust.edu.tw>
2024-05-26 01:57:10 +0800

    feat: 修改introduction排版 + 新增React Project

--- my-app/src/IntroductionPage/index.js
+++ my-app/src/IntroductionPage/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import '.././App.css'
 import BG_IMG from '../images/BG.png';
+import BG_CODE1 from '../images/BG-code1.jpg'
+// const BG_CODE1 = 'https://png.pngtree.com/thumb_back/fw800/background/20240125/pngtree-an-orange-cat-asleep-image_2499590.png'
+
+// Avatar
+import reactImg from '../images/react.png'
+import githubImg from '../images/github.png';
+import linkImg from '../images/link.png';
 
 const introduction = {
   cn_name: '蘇禎佑',
@@ -9,15 +16,20 @@ const introduction = {
   school: 'National Kaohsiung University of Science and Technology EE',
   company: 'BigGo 樂方股份有限公司',
   program_lan: 'JavaScript, Python, C, solidity',
-  experience: 'React, React Native, Extension, JS爬蟲, 智能合約'
+  experience: ['React', 'Extension', 'JS爬蟲', '智能合約', 'AI']
+  // experience: 'React, React Native, Extension, JS爬蟲, 智能合約'
 }
 
 export default function IntroductionPage() {
   return (
     <div className='IntroductionPage'>
-      <div className='IntroductionContent'>
-        <Avatar />
+      <div className='IntroductionContent' style={{backgroundImage: `url(${BG_IMG})`}}>
+        <Avatar image={BG_IMG} />
         <Introduction introduction={introduction} />
+      </div>
+      <div id="React" className='IntroductionContent' style={{backgroundImage: `url(${BG_CODE1})`}}>
+        <Avatar image={reactImg} width={'200px'} />
+        <ExperienceReact />
       </div>
       <div className='IntroductionSaying'>
         <div className='IntroductionSayingContent'>
@@ -30,10 +42,18 @@ export default function IntroductionPage() {
   )
 }
 
-function Avatar() {
+function Avatar(props) {
+  const { image, width } = props
+  const _padding = width
+    ? (400 - +width?.match(/([0-9]+)/)?.[1])/2
+    : 0
+  console.log('w', width)
+  const style = width 
+    ? { width: width, height: width, padding: `${_padding}px`}
+    : {}
   return (
-    <div className='Avatar'>
-      <img src={BG_IMG} />
+    <div className='Avatar' style={style}>
+      <img src={image}/>
     </div>
   )
 } 
@@ -70,10 +90,90 @@ function Introduction(props) {
           </tr>
           <tr>
             <th colSpan="6">PROJECT EXPERIENCE</th>
-            <td colSpan="4">{introduction.experience}</td>
+            {/* <td colSpan="4">{introduction.experience}</td> */}
+            <td colSpan="4">
+              {introduction.experience.map((item, index) => (
+                <span 
+                  key={index}
+                  style={{color: '#5B4B00', fontWeight: 'bold'}}
+                  onClick={() => document.querySelector(`#${item}`)?.scrollIntoView({ behavior: "smooth" })} 
+                  >{item}&nbsp;&nbsp;&nbsp;</span>
+              ))}
+            </td>
           </tr>
         </tbody>
       </table>
+    </div>
+  )
+}
+
+function ExperienceReact() {
+  return (
+    <div className='Introduction Experience'>
+      <div className='Introduction-title'>
+        <div>React Project</div>
+      </div>
+      <br/>
+      <div className='Introduction-subtitle'>
+        <div>WEB</div>
+      </div>
+      <table style={{borderTop: '1px black solid'}}>
+        <tbody>
+          <tr>
+            <th colSpan="6">PROJECT NAME</th>
+            <td colSpan="4">
+              <ExperienceProject 
+                title={"SocialMedia"} 
+                githubLink={"https://github.com/SuChenYu0623/SocialMedia"}
+                linkLink={"https://suchenyu0623.github.io/SocialMedia/"} />
+            </td>
+          </tr>
+          <tr>
+            <th colSpan="6">PROJECT NAME</th>
+            <td colSpan="4">
+              <ExperienceProject 
+                title={"Game"}
+                githubLink={"https://github.com/SuChenYu0623/Game"}
+                linkLink={"https://suchenyu0623.github.io/Game/"} />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      <br />
+      <div className='Introduction-subtitle'>
+        <div>APP</div>
+      </div>
+      <table style={{borderTop: '1px black solid'}}>
+        <tbody>
+          <tr>
+            <th colSpan="6">PROJECT NAME</th>
+            <td colSpan="4">
+              <ExperienceProject title={"RandomSelectMealApp"} githubLink={"https://github.com/SuChenYu0623/RandomSelectMealApp"} />
+            </td>
+          </tr>
+          <tr>
+            <th colSpan="6">PROJECT NAME</th>
+            <td colSpan="4">
+              <ExperienceProject title={"ReactNative_game_app"} githubLink={"https://github.com/SuChenYu0623/ReactNative_game_app"} />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+function ExperienceProject(props) {
+  const { title, githubLink, linkLink } = props
+  return (
+    <div className='ExperienceProject' >
+      {title}
+      <a className='ExperienceLink' style={{backgroundImage: githubImg}} href={githubLink} target="_blank" rel="noreferrer"></a>
+      {linkLink 
+        ? <a className='ExperienceLink' style={{backgroundImage: `url(${linkImg})`}} href={linkLink} target="_blank" rel="noreferrer"></a>
+        : <div></div>
+      }
     </div>
   )
 }
